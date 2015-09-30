e26c6ab26816f5fdcebb19d4599dca56c98cafbf
Anita <a.nita@hotmail.co.uk>
2015-09-30 12:01:26 +0100

    testing pass for home endpoint

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var Code = require('code');
 var Lab = require('lab');
 var Hapi = require('hapi');
 var Server = require('../lib/index.js');
+var Home = require('../lib/home.js');
 
 var lab = exports.lab = Lab.script();
 var expect = Code.expect;
@@ -18,4 +19,27 @@ it('starts server and return an instance of Hapi server', function (done) {
 
     server.stop(done);
   });
+
+});
+
+it('handles register plugin errors', { parallel: false }, function (done) {
+
+    Home.register = function (server, options, next) {
+
+        return next(new Error('failed plugin'));
+    };
+
+    Home.register.attributes = {
+
+        name: 'fake home plugin'
+    };
+
+    Server.init(0, function (err, server) {
+
+        expect(err).to.exist();
+        expect(err.message).to.equal('failed plugin');
+
+        done();
+    });
+    
 });
