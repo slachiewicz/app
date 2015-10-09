2d997cbcf19acbe94c82ab829cd58eb0baa8cec0
Anita <a.nita@hotmail.co.uk>
2015-10-09 11:52:46 +0100

    adding db to travis

---
+++ init/travisDb.js
@@ -0,0 +1,15 @@
+var es = require('./elasticSearch.js');
+var candidate = require(__dirname + '/../tmp/candidate.json');
+
+es.create({
+  index: 'globalm',
+  type: 'contacts',
+  id: 1,
+  body: candidate
+}, function (error, response) {
+  if(error) {
+      console.log(error);
+  }
+  console.log('initialize candidate with id ' + 1);
+});
+
