2705295d53a11403bf1f865177867e89a8ce8013
SimonLab <simon.labondance@gmail.com>
2015-11-04 22:32:29 +0000

    add level skill and update view

--- views/helpers/skillsHelper.js
+++ views/helpers/skillsHelper.js
@@ -1,3 +1,8 @@
 module.exports = function (skill) {
-   return "<li><span class='each'>" + skill + "</span></li>";
+    if(skill.level === 0) {
+
+        return "<li><span class='each'>" + skill.skill + "</span></li>";
+    } else {
+        return "<li><span class='each'>" + skill.level + " - " + skill.skill + "</span></li>";
+    }
 };
