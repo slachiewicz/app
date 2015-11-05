fad820e0f2d4cabb45f59ad9c14e51f552338a71
SimonLab <simon.labondance@gmail.com>
2015-11-05 15:41:07 +0000

    test api

--- test/api.js
+++ test/api.js
@@ -17,7 +17,7 @@ describe('api /profile', function () {
 
       expect(err).to.not.exist();
 
-      var profile = { 
+      var profile = {
         "url": "https://uk.linkedin.com/in/fakeprofile1",
         "connections": 500,
         "fullname": "David Dupont",
@@ -28,20 +28,20 @@ describe('api /profile', function () {
         "summary": "This is the summary of the first profile",
         "skills":[{ "level": 0, "skill": "Agile Methodologies" },{ "level": 0,"skill": "JavaScript" },{ "level": 0, "skill": "Node.js" }],
         "languages":[{"lang": "English", "fluency": "Native or bilingual proficiency"},{"lang" : "French" , "fluency": "Elementary proficiency" } ],
-        "experience":{ 
+        "experience":{
           "current": [{"title": "JS developer","org": "the best company","date": "October 2014 – Present (1 year 2 months)","desc": "I work as a developer and I'm creating some cool stuff","location": "London, United Kingdom"},{   "title": "Gardener","org": "The lovely tree","date": "October 2013 – Present (2 years 2 months)","desc": "I work as a gardener on my spare time","location": "London, United Kingdom"}],
           "past":[{  "title": "Guitar player","org": "Pink Floyd","date": "October 1984 – October 1985 (1 year)","desc": "I was a guitar hero","location": "The world"},{   "title": "Tennis player","org": "National French Team","date": "October 2010 – October 2013 (3 years)","desc": "I was the number 1","location": "Paris, France"}]}};
-      
+
       var options = {
         method: 'POST',
         url: '/profile',
         payload: profile
-      }
+    };
 
       server.inject(options, function (res) {
 
         expect(res.statusCode).to.equal(200);
-        
+
         setTimeout(server.inject('/candidate/1' , function (res) {
 
         expect(res.statusCode).to.equal(200);
