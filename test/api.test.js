39471436a85cd28eaec7ea01f751ed8bfe8e06de
Anita <a.nita@hotmail.co.uk>
2015-11-06 13:55:21 +0000

    testing api

--- test/api.test.js
+++ test/api.test.js
@@ -4,6 +4,7 @@ var Code = require('code');
 var Lab = require('lab');
 var Server = require('../lib/index.js');
 
+var cheerio = require('cheerio');
 var lab = exports.lab = Lab.script();
 var describe = lab.experiment;
 var expect = Code.expect;
@@ -46,6 +47,10 @@ describe('api /profile', function () {
 
         expect(res.statusCode).to.equal(200);
 
+        var $ = cheerio.load(res.payload);
+
+        expect($('.fullname').text()).to.equal("David Dupont");
+
         server.stop(done);
       }), 3000);
 
@@ -56,4 +61,46 @@ describe('api /profile', function () {
 
     });
   });
+
+    it('create profile', function (done) {
+
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+
+      var profile = {
+        "url": "https://uk.linkedin.com/in/newprofile",
+        "connections": 500,
+        "fullname": "Maria Dolores",
+        "location": "Manchaster, United Kingdom",
+        "current": "Super",
+        "picture": "https://static.licdn.com/scds/common/u/images/themes/katy/ghosts/person/ghost_person_150x150_v1.png",
+        "contacts": { "email": "fakecontact12@gmail.com","phone":"+44776777777 (Mobile)","im": [ "contactskype (SKYPE)", "ircContact (IRC)"],"address": "London" },
+        "summary": "This is the summary of the first profile",
+        "skills":[{ "level": 0, "skill": "Agile Methodologies" },{ "level": 0,"skill": "JavaScript" },{ "level": 0, "skill": "Node.js" }],
+        "languages":[{"lang": "English", "fluency": "Native or bilingual proficiency"},{"lang" : "French" , "fluency": "Elementary proficiency" } ],
+        "experience":{
+          "current": [{"title": "JS developer","org": "the best company","date": "October 2014 – Present (1 year 2 months)","desc": "I work as a developer and I'm creating some cool stuff","location": "London, United Kingdom"},{   "title": "Gardener","org": "The lovely tree","date": "October 2013 – Present (2 years 2 months)","desc": "I work as a gardener on my spare time","location": "London, United Kingdom"}],
+          "past":[{  "title": "Guitar player","org": "Pink Floyd","date": "October 1984 – October 1985 (1 year)","desc": "I was a guitar hero","location": "The world"},{   "title": "Tennis player","org": "National French Team","date": "October 2010 – October 2013 (3 years)","desc": "I was the number 1","location": "Paris, France"}]}};
+
+      var options = {
+        method: 'POST',
+        url: '/profile',
+        payload: profile
+    };
+
+      server.inject(options, function (res) {
+
+        expect(res.statusCode).to.equal(200);
+     
+        server.stop(done);
+    
+        // server.stop(done);
+
+      });
+
+
+    });
+  });
+
 });
