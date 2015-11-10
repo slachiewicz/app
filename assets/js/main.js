4b24ee09349505b8ea0f2eb0f62087cb46472058
SimonLab <simon.labondance@gmail.com>
2015-11-10 09:34:17 +0000

    comment filter button

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
     var searchInput = document.getElementById('search-box').value;
     var currentPage = document.getElementById('currentPage').value;
     var totalPages = document.getElementById('totalPages').value;
-    var filter = document.getElementById('filter').value;
-    console.log(filter);
+    //var filter = document.getElementById('filter').value;
+   var filter = 'all';    
 
     if (searchInput === '') {
       window.location.href = '/';
