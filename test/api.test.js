cb710d716df2f11d717df4ea55506451e057194f
SimonLab <simon.labondance@gmail.com>
2015-11-27 12:41:40 +0000

    resolve the issue https://github.com/FAC-GM/app/issues/112

--- test/api.test.js
+++ test/api.test.js
@@ -30,16 +30,16 @@ describe('api /profile', function () {
 
         expect(res.statusCode).to.equal(200);
 
-        var token =  JWT.sign({ id: 12, "name": "Simon", valid: true}, process.env.JWT_SECRET);
+        var tokenSimon =  JWT.sign({ id: '13', "name": "Simon", valid: true}, process.env.JWT_SECRET);
 
         var optionsCandidate = {
           method: "GET",
           url: "/candidate/" + res.payload,
-          headers: { cookie: "token=" + token }
+          headers: { cookie: "token=" + tokenSimon }
         };
 
         var redisClient = require('redis-connection')();
-        redisClient.set(12, JSON.stringify({ id: 12, "name": "Simon", valid: true}), function (err, res) {
+        redisClient.set(13, JSON.stringify({ id: 13, "name": "Simon", valid: true}), function (err, res) {
           setTimeout(server.inject(optionsCandidate , function (res) {
 
           expect(res.statusCode).to.equal(200);
@@ -47,6 +47,7 @@ describe('api /profile', function () {
           var $ = cheerio.load(res.payload);
 
           expect($('.fullname').text()).to.equal("David Dupont");
+          expect($('#star-state').attr('class')).to.equal('fa fa-star unchecked');
           server.stop(done);
 
           }), 3000);
@@ -55,6 +56,52 @@ describe('api /profile', function () {
       });
     });
   });
+
+    it('updates profile david and keep favourite', function (done) {
+
+      Server.init(0, function (err, server) {
+
+        expect(err).to.not.exist();
+
+        var profile = require('./fixtures/david-dupont.json');
+
+        var options = {
+          method: 'POST',
+          url: '/profile',
+          payload: profile
+      };
+
+        server.inject(options, function (res) {
+
+          expect(res.statusCode).to.equal(200);
+
+          var tokenSimon =  JWT.sign({ id: '12', "name": "Simon", valid: true}, process.env.JWT_SECRET);
+
+          var optionsCandidate = {
+            method: "GET",
+            url: "/candidate/" + res.payload,
+            headers: { cookie: "token=" + tokenSimon }
+          };
+
+          var redisClient = require('redis-connection')();
+          redisClient.set(12, JSON.stringify({ id: 12, "name": "Simon", valid: true}), function (err, res) {
+            setTimeout(server.inject(optionsCandidate , function (res) {
+
+            expect(res.statusCode).to.equal(200);
+
+            var $ = cheerio.load(res.payload);
+
+            expect($('.fullname').text()).to.equal("David Dupont");
+            //the yellow star should be display
+            expect($('#star-state').attr('class')).to.equal('fa fa-star');
+            server.stop(done);
+
+            }), 3000);
+
+          });
+        });
+      });
+    });
 
 
   it('updates profile manuel same 1st degree contact', function (done) {
