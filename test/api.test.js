f132d3bc6c00333016880f522ebef466b909df28
Anita <a.nita@hotmail.co.uk>
2016-02-11 18:11:02 +0000

    styled button send-email on candidate view

--- test/api.test.js
+++ test/api.test.js
@@ -94,7 +94,6 @@ describe('api /profile', function () {
 
             expect($('.fullname').text()).to.equal("David Dupont");
             //the yellow star should be display
-            console.log($('#star-state').attr('class'));
             expect($('#star-state').attr('class')).to.equal('fa fa-star');
             server.stop(done);
 
