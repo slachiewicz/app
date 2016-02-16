fffec1cf0e12a9827cdc6ffcf5e955cb9c3ee6eb
Anita <a.nita@hotmail.co.uk>
2016-02-16 10:29:48 +0000

    added test for sending_email helper and fixed nock issue see https://github.com/FAC-GM/app/issues/253

--- test/sendemailHelper.test.js
+++ test/sendemailHelper.test.js
@@ -1,41 +1,63 @@
-// require('env2')('.env');
-// var Code = require('code');
-// var Lab = require('lab');
-// var Server = require('../lib/index.js');
-// // var nock = require('nock');
+require('env2')('.env');
+var Code = require('code');
+var Lab = require('lab');
+var Server = require('../lib/index.js');
+var sendEmail = require('../lib/helpers/sendemail_gmail.js');
 
-// var sendEmail = require('../lib/helpers/sendemail_gmail.js');
-
-// var lab = exports.lab = Lab.script();
-// var describe = lab.experiment;
-// var expect = Code.expect;
-// var it = lab.test;
-// var fs = require('fs');
+var lab = exports.lab = Lab.script();
+var describe = lab.experiment;
+var expect = Code.expect;
+var it = lab.test;
+var fs = require('fs');
 // var REAL_TOKEN = require(__dirname + '/fixtures/sample-auth-token-anita.json');
+var PROFILE = require(__dirname + '/fixtures/sample-auth-credentials.json');
 
-// var date = new Date().toUTCString();
+var date = new Date().toUTCString();
 
-// describe('Attempt to sendEmail with expired token', function () {
+describe('Attempt to sendEmail with the expired token', function () {
 
-//   it('not succesfully send email with real token', function (done) {
+  it('return error code 400', function (done) {
 
-//     var email = 'dolores.maria9810@gmail.com';
+    var email = 'dolores.maria9810@gmail.com';
     
-//     var tokens = REAL_TOKEN;
+    var tokens = PROFILE.tokens;
 
-//     var options = {
-//       name: 'anita',
-//       senderEmail: 'czaplaanita@gmail.com',
-//       subject: 'Do You Read Me? > ' + date,
-//       message: 'Hi there'      
-//     }
+    var options = {
+      name: 'anita',
+      senderEmail: 'czaplaanita@gmail.com',
+      subject: 'Do You Read Me? > ' + date,
+      message: 'Hi there'      
+    }
 
-//     sendEmail(email, options, tokens, function (err, response) {
-//       console.log('ERR',err);
-//       console.log('RS',response);
-//       // expect(response.labelIds[0]).to.equal('SENT');
-//       expect(err).to.equal(400);
-//       done();
-//     });
-//   });
-// });
+    sendEmail(email, options, tokens, function (err, response) {
+      expect(err['code']).to.equal(400);
+      done();
+    });
+  });
+});
+
+
+describe('Attempt to sendEmail with the right token', function () {
+
+  it('succesfully send email with real token', function (done) {
+
+    var email = 'dolores.maria9810@gmail.com';
+    
+    var tokens = JSON.parse(process.env.REAL_TOKEN);
+
+    var options = {
+      name: 'anita',
+      senderEmail: 'czaplaanita@gmail.com',
+      subject: 'Do You Read Me? > ' + date,
+      message: 'Hi there'      
+    }
+
+    sendEmail(email, options, tokens, function (err, response) {
+      console.log('ERR',err);
+      console.log('RS',response);
+      expect(response.labelIds[0]).to.equal('SENT');
+      expect(err).to.equal(null);
+      done();
+    });
+  });
+});
